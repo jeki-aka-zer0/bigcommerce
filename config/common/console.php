696c046fd41d6969a7851c1e24f79effa51dd931
Dmitriy Mironov <mironovdv@manychat.com>
2020-09-15 20:23:23 +0300

    Cart redirect

--- config/common/console.php
+++ config/common/console.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Psr\Container\ContainerInterface;
-use Src\Core\Domain\Model\Auth\IntegrationRepository;
 use Src\Core\Domain\Model\Cart\CartRepository;
 use Src\Core\Domain\Model\CartSession\CartSessionRepository;
 use Src\Core\Domain\Model\Job\JobProcessor;
 use Src\Core\Domain\Model\Job\JobRepository;
-use Src\Core\Infrastructure\Domain\Model\ClientConfigurator;
 use Src\Core\Infrastructure\Ui\Console\Command\JobsCommand;
 
 return [
@@ -24,8 +22,7 @@ return [
             new JobProcessor(
                 $c->get(CartSessionRepository::class),
                 $c->get(CartRepository::class),
-                $c->get(IntegrationRepository::class),
-                $c->get(ClientConfigurator::class)
+                $c->get('config')['main']['domain']
             ),
             $logger,
         );
